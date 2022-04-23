5e9b76367c40143a352077cf5586e57f76feea04
camilomonje <lumonje12@gmail.com>
2022-04-23 12:13:30 -0500

    TestFindById

--- src/test/java/com/foodka/back/services/implement/ReservaImplementServiceTest.java
+++ src/test/java/com/foodka/back/services/implement/ReservaImplementServiceTest.java
@@ -6,6 +6,7 @@ import com.foodka.back.services.ReservaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -22,6 +23,15 @@ class ReservaImplementServiceTest {
         ReservaDTO reserva = new ReservaDTO( "","12:00","23/04/2022",new Cliente());
         Mono<ReservaDTO> reserva2 = service.save(reserva);
         StepVerifier.create(reserva2).expectNext(reserva).verifyComplete();
+    }
+
+    @Test
+    void testFindById() {
+        ReservaDTO reserva = new ReservaDTO( "","12:00","23/04/2022",new Cliente());
+        Mono<ReservaDTO> reservaDTOMono = service.save(reserva);
+        String id = reservaDTOMono.block().getId();
+        Mono<ReservaDTO> reservaDTOMono1 = service.findById(id);
+        StepVerifier.create(reservaDTOMono1).expectNext(reserva).verifyComplete();
     }
 
 }
