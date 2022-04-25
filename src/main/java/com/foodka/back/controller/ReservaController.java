fe52187721c951a3396465582ba3075a5d28bacc
camilomonje <lumonje12@gmail.com>
2022-04-25 18:13:49 -0500

    Se cambia origins en @CrossOrigin

--- src/main/java/com/foodka/back/controller/ReservaController.java
+++ src/main/java/com/foodka/back/controller/ReservaController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/reserva")
 public class ReservaController {
 
